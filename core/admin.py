68383ed9ec773913a8e1b5fe3d46102bada26e91
Daniel <dancarsoa@gmail.com>
2021-10-18 16:35:56 -0300

    Segundo commit

--- core/admin.py
+++ core/admin.py
@@ -3,4 +3,8 @@ from core.models import Evento
 
 # Register your models here.
 
-admin.site.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('usuario','data_evento')
+
+admin.site.register(Evento, EventoAdmin)
